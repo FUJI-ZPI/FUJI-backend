80160fa0a3fa60071b63d09963902b855adf2e2d
Tomek4861 <tomek4861g@gmail.com>
2025-11-03 22:30:07 +0100

    add request logging filter & add minio do docker-compose.yml & fix containerization

---
+++ src/main/java/com/zpi/fujibackend/config/http/RequestLoggingFilter.java
@@ -0,0 +1,22 @@
+package com.zpi.fujibackend.config.http;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class RequestLoggingFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        log.info("Request URL: {} {}", httpRequest.getMethod(), httpRequest.getRequestURL());
+        chain.doFilter(request, response);
+    }
+}
+
