990a8f807a7a59ca4956f135b44e27ef319e0ec3
Tomek4861 <tomek4861g@gmail.com>
2025-11-12 17:16:25 +0100

    update radicals table to set uuid as unique with default value

--- src/main/resources/db/migration/V7__create_radicals_table.sql
+++ src/main/resources/db/migration/V7__create_radicals_table.sql
@@ -4,6 +4,6 @@ CREATE TABLE IF NOT EXISTS wanikani.radicals (
     character VARCHAR(255),
     character_unicode VARCHAR(10),
     slug VARCHAR(255) NOT NULL,
-    uuid UUID NOT NULL,
+    uuid UUID NOT NULL UNIQUE DEFAULT gen_random_uuid(),
     document JSONB
 );
